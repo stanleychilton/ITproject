f405d6b7169d9c9b54d78a1a0921e649b0df8a85
stan surface <stanleychilton@live.com>
2019-07-25 16:31:42 +1200

    Update csvopener.py
    added a check to stop the file throwing an error whenm unsupported files were chosen

--- csvopener.py
+++ csvopener.py
@@ -3,19 +3,24 @@ import csv, os
 files = os.listdir()
 for x in range(0, len(files)):
     print(x+1, files[x])
+while True:
+    i = int(input("please choose a csv file to read: "))
+    chosenfile = files[i-1]
 
-i = int(input("please choose a csv file to read"))
-chosenfile = files[i-1]
 
-with open(chosenfile) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            row[0] = (row[0])[3:]
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} {row[1]} {row[2]} {row[3]}')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+    try:
+        with open(chosenfile) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    row[0] = (row[0])[3:]
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                else:
+                    print(f'\t{row[0]} {row[1]} {row[2]} {row[3]}')
+                    line_count += 1
+            print(f'Processed {line_count} lines.')
+            break
+    except:
+        print("file not supported")
