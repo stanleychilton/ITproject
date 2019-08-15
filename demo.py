5659a6dde2b066595e26d11db2f13bb5aef684e5
stan surface <stanleychilton@live.com>
2019-08-15 16:21:28 +1200

    Update demo.py
    updating details of demo to have relatively correct assignment parts

--- demo.py
+++ demo.py
@@ -3,12 +3,13 @@ import smtplib, ssl
 port = 587  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "codfisharecool987@gmail.com"  # Enter your address
-receiver_email = "stanleychilton@live.com"  # Enter receiver address
+receiver_email = row[3]  # Enter receiver address
 password = "CodFish123"
 message = """\
-Subject: Hi there
+Subject: Massey 158.120 moodle link
 
-This message is sent from Python."""
+Here is the link to your live version of moodle.
+www.clicker-box.com"""
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
