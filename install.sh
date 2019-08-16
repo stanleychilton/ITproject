4cc57b00af65f3669c7e2e211e60bafb79e493c9
fish258 <44438335+fish258@users.noreply.github.com>
2019-08-16 14:15:43 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,4 +52,4 @@ sudo expect ~/secure_our_mysql.sh
 rm -v ~/secure_our_mysql.sh # Remove the generated Expect script
 #sudo apt-get -qq purge expect > /dev/null # Uninstall Expect, commented out in case you need Expect
 
-echo "MySQL setup completed. Insecure defaults are gone. Please remove this script manually when you are done with it (or at least remove the MySQL root password that you put inside it."
+echo "MySQL setup completed."
