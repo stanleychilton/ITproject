10e10217b097e74235dde63c57bc0881a2f6162d
fish258 <44438335+fish258@users.noreply.github.com>
2019-08-16 15:10:35 +1200

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo su - root -c "echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections"
-#sudo su - root -c "echo "phpmyadmin phpmyadmin/dbconfig-install boolean false" | debconf-set-selections"
+#sudo su - root -c "echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections"
 #sudo apt-get -y install phpmyadmin
 
 sudo apt-get install pwgen -y
