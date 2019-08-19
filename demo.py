3f8d996ef1b263780c6fd32e958dfdd33bca048f
stan surface <stanleychilton@live.com>
2019-08-19 15:46:27 +1200

    Update demo.py
    commit for delete

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import smtplib, ssl
 port = 587  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "codfisharecool987@gmail.com"  # Enter your address
-receiver_email = row[3]  # Enter receiver address
+receiver_email = "stanleychilton@live.com"  # Enter receiver address
 password = "CodFish123"
 message = """\
 Subject: Massey 158.120 moodle link
