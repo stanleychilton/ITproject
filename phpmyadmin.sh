8457fb36691611f169e0e4c773d83e10540cedab
stanley chilton <stanleychilton@live.com>
2019-08-25 15:50:30 +1200

    Delete phpmyadmin.sh

--- phpmyadmin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo su - root -c "echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections"
-sudo su - root -c "echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections"
-sudo apt-get -y install phpmyadmin
