8e1546ae5cbcd99ebe82be41701436d7953a4197
stanleys desktop\Stanley Chilton <stanleychilton@live.com>
2019-09-18 13:39:32 +1200

    email updates and testing
    changed the name of the email script due to clashes in the packages being imported for th etesting script, testing script finished and tested needs comms with eva about smtp settings for the massey system

--- emailtestingscript.py
+++ emailtestingscript.py
@@ -5,8 +5,8 @@ from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-MY_ADDRESS = 'my_address@example.comm'  ################## going to change this to a user input depending on the outcome
-PASSWORD = 'mypassword'                 ################## of testing so that theres no hardcoded usernames and passwords
+MY_ADDRESS = 'testyacount82@yahoo.com'  ################## going to change this to a user input depending on the outcome
+PASSWORD = 'acounttest'                 ################## of testing so that theres no hardcoded usernames and passwords
 
 
 def get_contacts(filename):
@@ -36,35 +36,37 @@ def read_template(filename):
 
 
 def main():
-    names, emails = get_contacts('mycontacts.txt')  # read contacts
-    message_template = read_template('message.txt')
+    #names, emails = get_contacts('mycontacts.txt')  # read contacts
+    #message_template = read_template('message.txt')
 
     # set up the SMTP server
-    s = smtplib.SMTP(host='your_host_address_here', port=your_port_here) ###### edit this line
+    s = smtplib.SMTP(host='smtp.mail.yahoo.com', port=587) ###### edit this line
+    print("test")
     s.starttls()
+
+
     s.login(MY_ADDRESS, PASSWORD)
 
     # For each contact, send the email:
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()  # create a message
+    #for name, email in zip(names, emails):
+    msg = MIMEMultipart()  # create a message
 
-        # add in the actual person name to the message template
-        message = message_template.substitute(PERSON_NAME=name.title())
+    # add in the actual person name to the message template
+    message = "test mail"#message_template.substitute(PERSON_NAME=name.title())
 
-        # Prints out the message body for our sake
-        print(message)
+    # Prints out the message body for our sake
+    print(message)
 
-        # setup the parameters of the message
-        msg['From'] = MY_ADDRESS
-        msg['To'] = email
-        msg['Subject'] = "This is TEST"
+    # setup the parameters of the message
+    msg['From'] = MY_ADDRESS
+    msg['To'] = 'stanleychilton@live.com'
+    msg['Subject'] = "This is TEST"
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
 
-        # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
-
-        # send the message via the server set up earlier.
-        s.send_message(msg)
-        del msg
+    # send the message via the server set up earlier.
+    s.send_message(msg)
+    del msg
 
     # Terminate the SMTP session and close the connection
     s.quit()
