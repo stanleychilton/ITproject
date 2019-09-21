da76cb73609c247461a4a692d2b677e8acf3dd27
stanleys desktop\Stanley Chilton <stanleychilton@live.com>
2019-09-21 16:10:55 +1200

    Update NewCreator.py
    updating with the database link

--- NewCreator.py
+++ NewCreator.py
@@ -2,7 +2,7 @@ import boto3
 import csv
 import time
 import paramiko
-
+import requests
 
 client = boto3.client('ec2')
 ec2 = boto3.resource('ec2')
@@ -16,6 +16,8 @@ with open('test.csv') as csv_file:
             line_count += 1
         else:
             print(row)
+            userdata = {"firstname": row[1], "lastname": row[2], "email": row[3]}
+            resp = requests.post('http://www.clicker-box.com/selectionsite/users.php', params=userdata)
             line_count += 1
 
 
