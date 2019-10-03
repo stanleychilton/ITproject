84040194d231744a473970ae2f3ba0da24051f20
Rahtz <47646398+Rahtz@users.noreply.github.com>
2019-10-03 18:05:24 +1300

    Update instances.py

--- instances.py
+++ instances.py
@@ -1,4 +1,16 @@
 import requests
-userdata = {"url": "www.instance4.com", "id": instance_id}
-resp = requests.post('http://www.clicker-box.com/selectionsite/instances.php', params=userdata)
-print(resp)
+import boto3
+ec2 = boto3.resource('ec2')
+
+l = []
+
+for i in ec2.instances.all():
+     l.append([i.public_ip_address, i.instance_id])
+     l = list(filter(None, l))
+     
+
+
+for j in l:
+     userdata = {"url": j[0], "id":j[1]}
+     resp = requests.post('http://www.clicker-box.com/selectionsite/instances.php', params=userdata)
+     print(resp)
