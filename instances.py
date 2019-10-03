e210d52335d7a79d4a4ebfe2a1bdae36ada00456
stan surface <stanleychilton@live.com>
2019-10-03 18:02:27 +1300

    Update instances.py

--- instances.py
+++ instances.py
@@ -1,4 +1,4 @@
 import requests
-userdata = {"url": "www.instance4.com"}
+userdata = {"url": "www.instance4.com", "id": instance_id}
 resp = requests.post('http://www.clicker-box.com/selectionsite/instances.php', params=userdata)
 print(resp)
